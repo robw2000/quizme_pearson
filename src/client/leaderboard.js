75a72f0185ef06a060a89d93718016b6f8d92176
Rob Wilburn <rob@mathist.net>
2013-11-06 14:06:56 -0700

    Added s at the end of time taken on leaderboard

--- src/client/leaderboard.js
+++ src/client/leaderboard.js
@@ -4,9 +4,6 @@ Template.leaderboard_allgames.helpers({
   },
   isAllWinnerClass: function(isAllWinner) {
     return (isAllWinner) ? 'icon-trophy' : '';
-  },
-  display_time: function(time) {
-    return time + 's';
   }
 });
 
@@ -16,9 +13,6 @@ Template.leaderboard_recentgames.helpers({
   },
   isRecentWinnerClass: function(isRecentWinner) {
     return (isRecentWinner) ? 'icon-trophy' : '';
-  },
-  display_time: function(time) {
-    return time + 's';
   }
 });
 
